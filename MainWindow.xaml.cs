707d489b6ad75e24dff3f75b41bcc5b095038176
Hallworld <prishedko.ilya@yandex.ru>
2025-05-25 14:06:56 +0300

    asd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace LagrangePolynomial
     public partial class MainWindow : Window
     {
         private ViewModel _vm = new ViewModel();
+        private System.Windows.Shapes.Line _axisX;
+        private System.Windows.Shapes.Line _axisY;
         public MainWindow()
         {
             InitializeComponent();
@@ -25,9 +27,22 @@ namespace LagrangePolynomial
             Dots.ItemsSource = _vm.Dots;
             Lines.ItemsSource = _vm.Lines;
             this.Resources.Add("CircleSize", _vm.CircleSize);
+
+            _axisX = new System.Windows.Shapes.Line() { X1 = 0, X2 = 1000, Y1 = 500, Y2 = 500, 
+                Stroke = new SolidColorBrush(Colors.Black), StrokeThickness = 2};
+            _axisY = new System.Windows.Shapes.Line() { X1 = 500, X2 = 500, Y1 = 0, Y2 = 1000,
+                Stroke = new SolidColorBrush(Colors.Black), StrokeThickness = 2};
+            this.SizeChanged += MainWindow_SizeChanged;
+            MainCanvas.Children.Add(_axisX);
+            MainCanvas.Children.Add(_axisY);
         }
 
-     
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this._axisX.X2 = e.NewSize.Width;
+            this._axisY.Y2 = e.NewSize.Height;
+        }
+
         private void MainCanvas_MouseClicked(object sender, MouseButtonEventArgs e)
         {
             var p = e.GetPosition(MainCanvas);
